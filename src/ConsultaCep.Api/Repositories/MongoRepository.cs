e883a378de5c71c19e5e217c34042f8b246fac1a
olliverDev <pla.olliver@gmail.com>
2020-03-04 00:23:25 -0400

    Remoção de código não utilizado

--- src/ConsultaCep.Api/Repositories/MongoRepository.cs
+++ src/ConsultaCep.Api/Repositories/MongoRepository.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace ConsultaCep.Api.Repository
